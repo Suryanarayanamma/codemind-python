77be8c15914ae3b8934e85660dc4fec31040f8f6
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-08-14 20:37:58 +0530

    ch=input() if(ch=='V'or ch=='v'):     print("Violet") elif(ch=='I'or ch=='i'):     print("Indigo") elif(ch=='B'or ch=='b'):     print("Blue") elif(ch=='G'or ch=='g'):     print("Green") elif(ch=='Y'or ch=='y'):     print("Yellow") elif(ch=='O'or ch=='o'):     print("Orange") elif(ch=='R'or ch=='r'):     print("Red") else:     print("-1")

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+ch=input()
+if(ch=='V'or ch=='v'):
+    print("Violet")
+elif(ch=='I'or ch=='i'):
+    print("Indigo")
+elif(ch=='B'or ch=='b'):
+    print("Blue")
+elif(ch=='G'or ch=='g'):
+    print("Green")
+elif(ch=='Y'or ch=='y'):
+    print("Yellow")
+elif(ch=='O'or ch=='o'):
+    print("Orange")
+elif(ch=='R'or ch=='r'):
+    print("Red")
+else:
+    print("-1")
