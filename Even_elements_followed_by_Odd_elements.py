e7b71f8e757fffe89ef4817e68d08882e8e6776e
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-10-15 09:23:37 +0530

    n=int(input()) k=list(map(int,input().split())) l=[] for i in k:     if i%2==0:         l.append(i) for i in k:     if i%2!=0:         l.append(i) for j in l:     print(j,end=' ')

---
+++ Even_elements_followed_by_Odd_elements.py
@@ -0,0 +1,11 @@
+n=int(input())
+k=list(map(int,input().split()))
+l=[]
+for i in k:
+    if i%2==0:
+        l.append(i)
+for i in k:
+    if i%2!=0:
+        l.append(i)
+for j in l:
+    print(j,end=' ')
