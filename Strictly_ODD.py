3ce4a63c993801b90c56db1413a2b968c80177da
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-10-22 08:24:15 +0530

    n=int(input()) l=list(map(int,input().split())) k=[] s=[] for i in range(n):     if i%2!=0 and l[i]%2!=0:         k.append(i) for i in l:     if i%2!=0:         s.append(i) if len(k)==len(s):     print('True') else:     print("False")

---
+++ Strictly_ODD.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=list(map(int,input().split()))
+k=[]
+s=[]
+for i in range(n):
+    if i%2!=0 and l[i]%2!=0:
+        k.append(i)
+for i in l:
+    if i%2!=0:
+        s.append(i)
+if len(k)==len(s):
+    print('True')
+else:
+    print("False")
