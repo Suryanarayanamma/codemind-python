727ca22fbe2dab2d2bdc4e014852b4563fd750b4
Suryanarayanamma <119720732+Suryanarayanamma@users.noreply.github.com>
2023-10-23 20:36:53 +0530

    def is_reverse(n):     s=0     while n>0:         r=n%10         s=s*10+r         n=n//10     return s n=int(input()) sq=n*n x=is_reverse(n) sq1=x*x if sq==is_reverse(sq1):     print("True") else:     print("False")

--- Adam_number.py
+++ Adam_number.py
@@ -1,15 +1,15 @@
-def reverse(n):
-    rev=0
-    while n:
-        d=n%10
+def is_reverse(n):
+    s=0
+    while n>0:
+        r=n%10
+        s=s*10+r
         n=n//10
-        rev=rev*10+d
-    return rev
+    return s
 n=int(input())
 sq=n*n
-rev=reverse(n)
-sq1=rev*rev
-if sq==reverse(sq1):
-    print('True')
+x=is_reverse(n)
+sq1=x*x
+if sq==is_reverse(sq1):
+    print("True")
 else:
-    print('False')
+    print("False")
